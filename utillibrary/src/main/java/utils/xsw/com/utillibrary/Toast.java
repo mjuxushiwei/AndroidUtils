21a6949c72d485a161a50dc7c538c306379f6112
xushiwei <https://github.com/mjuxushiwei/AndroidUtils.git>
2018-09-18 18:52:12 +0800

    修改

--- utillibrary/src/main/java/utils/xsw/com/utillibrary/Toast.java
+++ utillibrary/src/main/java/utils/xsw/com/utillibrary/Toast.java
@@ -10,7 +10,7 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 
-public class wToast {
+public class Toast {
     public static final int LENGTH_LONG = 3500;  
     public static final int LENGTH_SHORT = 2000;  
     private WindowManager mWindowManager;
